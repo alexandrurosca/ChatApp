0c786abbf07a02b9eff190e0ed9a9d4dbd6df104
Alex <alex.rosca1996@gmail.com>
2017-12-01 16:45:10 +0200

    database first operation

---
+++ public/databaseMongo/db.js
@@ -0,0 +1,46 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+
+exports.addUser = function (obj) {
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        db.collection("users").insertOne(obj, function (err, res) {
+            if (err) throw err;
+            console.log("one user  inserted ", res);
+            db.close();
+        });
+    });
+}
+
+exports.findUsername = function(username, callback ){
+    var foundUser = false;
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var query = { username: username };
+        db.collection("users").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            foundUser = ((result.length != 0) ? true : false);
+            db.close();
+            callback(foundUser);
+        });
+    });
+}
+
+exports.findUser = function(username,password, callback ){
+    var foundUser = false;
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var query = { username: username, password: password};
+        db.collection("users").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            foundUser = ((result.length != 0) ? true : false);
+            db.close();
+            callback(foundUser);
+        });
+    });
+}
+
+
+
--- routes/log.js
+++ routes/log.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var database = require('../public/databaseMongo/db.js');
 router.get('/', function(req, res){
     res.render('./log.ejs');
 });
 router.post('/', function(req, res){
-    res.render('./chat.ejs');
+
 
     //add validations
 
@@ -14,7 +15,25 @@ router.post('/', function(req, res){
         username : req.body.userName,
         CNP : 1234567891234,
         password: req.body.password };
-    console.log(User);
+
+    // database.findUsername("biancafloriana", function (found) {
+    //     ((found) ? console.log("This username already exist"): console.log("This is a new username"));
+    // });
+
+    database.findUser(User.username, User.password, function (found) {
+        if(found){
+            res.render('./chat.ejs');
+            console.log("You can go on chat page!");
+        }else{
+            res.render('./log.ejs');
+            console.log("Your username / password is incorrect!");
+        }
+
+    } );
+
+
+
+
     });
 
 module.exports = router;
