ce8091bd50885fe58d10aaabfc9c872bb094dddb
biancafloriana07 <biancafloriana07@gmail.com>
2017-12-03 16:16:38 +0200

    +room try 1

--- public/JS/formCreateAccHandler.js
+++ public/JS/formCreateAccHandler.js
@@ -1,11 +1,26 @@
-var express = require('express');
+//var express = require('express');
 var database = require('../databaseMongo/db.js');
 
 exports.form = function (req,res){
 
-    //add validations
+    //validations
     req.checkBody('userName','Must insert a user!').notEmpty();
+    req.checkBody('userName','Incorect user name format!Use just alphanumeric characters!').isAlphanumeric();
+    req.checkBody('userName','User name length minim 5!').isLength({min:5});
+
     req.checkBody('password','Must insert a password!').notEmpty();
+    req.checkBody('password','Password length minim 8!').isLength({min:8});
+    req.checkBody('userName','Incorect password format!Use just alphanumeric characters!').isAlphanumeric();
+
+    req.checkBody('firstName','Must insert your fisrt name!').notEmpty();
+    req.checkBody('firstName','Incorect first name format !').isAlpha();
+
+    req.checkBody('lastName','Must insert your last name!').notEmpty();
+    req.checkBody('firstName','Incorect last name format!').isAlpha();
+
+    req.checkBody('CNP','Must insert a your CNP!').notEmpty();
+    req.checkBody('CNP','Incorect CNP format!').isNumeric();
+    req.checkBody('CNP','Password length minim 8!').isLength({min:3});
 
     var errors = req.validationErrors();
     console.log(errors);
