91fb1afd4e1a23069ba7608f5bcd58b844592de8
Alex <alex.rosca1996@gmail.com>
2017-12-12 13:08:49 +0200

    list of friends

--- public/JS/ChatTemplateHandler.js
+++ public/JS/ChatTemplateHandler.js
@@ -21,16 +21,12 @@ $(function () {
             alert(data);
         })
     })
-
-
 });
 
 
-
-    $('#add').submit(function(){
-       var nameFriend = document.getElementById('nameFriend');
-       document.alert(nameFriend);
-});
-
-
+// When the user clicks on <div>, open the popup
+// function myFunction() {
+//     var popup = document.getElementById("myPopup");
+//     popup.classList.toggle("show");
+// }
 
