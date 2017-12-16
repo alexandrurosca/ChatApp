4a3f708d8397a8b3b51300bad6be789fa251e1ea
biancafloriana07 <biancafloriana07@gmail.com>
2017-12-16 10:41:20 +0200

    list of friends

--- public/JS/ChatTemplateHandler.js
+++ public/JS/ChatTemplateHandler.js
@@ -1,8 +1,12 @@
 //var database = require('../databaseMongo/db.js');
 // $.import_js('/path_to_project/scripts/somefunctions.js');
 // var cookieParser = require('./cookieHandler');
-
+var username;
+var socket;
 $.getScript('JS/cookieHandler.js', function() {
+     username = getCookie("user");
+     console.log(username);
+     socket.emit('friendsList',username);
 });
 
 $(document).ready(function(){
@@ -10,12 +14,12 @@ $(document).ready(function(){
            });
 
 $(function () {
-    //var cookieParser = require('./cookieHandler');
-    var socket = io();
+
+    socket = io();
 
     $('#addFriends').submit(function () {
         var friendName =$('#nameFriendInput').val();
-        var username = getCookie("user");
+
         socket.emit('add friend', username, friendName );
         socket.on('confirm add friend', function (data) {
             alert(data);
@@ -23,8 +27,13 @@ $(function () {
     })
 
 
-});
+    socket.on('friendsList',function (friends) {
 
+        for(var  i=1;i<friends.length;i++){
+            $('#friendsList').innerHTML+= "<div class=\"row friend\" id='Friend1' friends[i].val();</div>";
+        }
+    });
+});
 
 
     $('#add').submit(function(){
