2032f8d605f66fed38107e3160807510111774f9
biancafloriana07 <biancafloriana07@gmail.com>
2017-12-17 13:32:16 +0200

    room change + listenr last button problem

--- public/JS/ChatTemplateHandler.js
+++ public/JS/ChatTemplateHandler.js
@@ -3,6 +3,7 @@
 // var cookieParser = require('./cookieHandler');
 var username;
 var socket;
+
 $.getScript('JS/cookieHandler.js', function() {
      username = getCookie("user");
      console.log(username);
@@ -11,12 +12,21 @@ $.getScript('JS/cookieHandler.js', function() {
 
 $(document).ready(function(){
        //  $('#chat').load("./chat");
+    console.log(window.myhead);
            });
 
 $(function () {
-
+    var openedRoom;
     socket = io();
-    //nuuuuu
+
+    $('#chat').submit(function(){
+        var data = {name:username, msg: $('#m').val(), room: openedRoom };
+        socket.emit('chat message', data);
+        $('#m').val('');
+        return false;
+    });
+
+
     $('#addFriends').submit(function () {
         var friendName =$('#nameFriendInput').val();
 
@@ -30,28 +40,52 @@ $(function () {
     socket.on('friendsList',function (friends) {
         var friendsList = document.getElementById('friendsList');
         try{
-                var lenghtF = friends.length;
-            for(var  i=0;i<lenghtF;i++) {
+
+            for(var  i = 0;i<friends.length;i++) {
                 friendsList.innerHTML += "<div class=\"row friend\" id=" + i + ">" + friends[i] + "</div>";
-                var friend = document.getElementById(i.toString());
-                friend.addEventListener('click',function(){
-                    console.log("yes");
-                    socket.emit('createConversation',username,friend.innerText);
-                });
-            }}
+            }
+
+            for(var  i = 0;i<friends.length;i++) {
+                var friend = document.getElementById(i);
+
+                 friend.addEventListener('click',function(){
+                     console.log( this.innerText);
+
+                     socket.emit('createConversation',username,this.innerText);
+
+                     try {var openedConversation = document.getElementById(username+this.innerText).innerText;
+                     }
+                     catch(err) {
+                         var openedConversations = document.getElementById('conversations');
+                         openedConversations.innerHTML +=
+                             "<div class=\"col-2 conversationFriend\" > <input id=" + username + this.innerText +
+                             " class='change' type=\"button\" value=" + this.innerText + "></div>"
+                         openedRoom = username + this.innerText;
+
+                         var conversation = document.getElementById(username + this.innerText);
+                            console.log(conversation.id);
+                        conversation.addEventListener('click',function(){
+                            openedRoom = this.id;
+
+                        })
+
+
+                     }
+                     });
+            }
+        }
         catch(err) {
             friendsList.innerHTML += "<div class=\"row friend\" id=" + i + ">No friends:( </div>";
             //console.log(err.message);
         }
-
     });
 
-    socket.on('createConversation',function () {
+    socket.on('createConversation',function (room) {
         $('#chat').load("./chat");
     });
 
     socket.on('connectToRoom',function (friend,thisUsername) {
-        console.log('client part '+ friend);
+      //  console.log('client part '+ friend);
         console.log(thisUsername);
         if (thisUsername==username)
                 socket.emit('connectToRoom',friend+thisUsername);
@@ -63,8 +97,6 @@ $(function () {
        var nameFriend = document.getElementById('nameFriend');
        document.alert(nameFriend);
 }
-
-
 
 );
 
--- public/JS/chatHandler.js
+++ public/JS/chatHandler.js
@@ -3,12 +3,7 @@ $(function () {
     var socket = io();
     var username = getCookie("user");
 
-    $('form').submit(function(){
-        var data = {name:username, msg: $('#m').val()};
-        socket.emit('chat message', data);
-        $('#m').val('');
-        return false;
-    });
+
     socket.on('chat message', function(msg){
         $('#messages').append($('<li>').text(msg.name + ":" +msg.msg));
         $('#msg').scrollTop($('#msg')[0].scrollHeight);
